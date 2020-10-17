1939a5cec66dbf8446390e7b3057a725aec56854
Amyh97 <amy.hollis@btinternet.com>
2020-10-17 12:36:54 +0000

    add carousel and cards to browse.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect
 from os import path
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -9,4 +9,11 @@ if path.exists("env.py"):
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'task_manager'
 MONGO_URI = os.environ.get("MONGO_URI")
+app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/browse_recipes')
+def browse_recipes():
+    return render_template('browse.html', recipes=mongo.db.recipes.find())
