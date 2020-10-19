cf8c943d054f9e9bef7749ce694e3053682000e3
Amyh97 <amy.hollis@btinternet.com>
2020-10-19 12:20:50 +0000

    fix syntax with {}, add search and add pages

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_DBNAME'] = 'recipes'
 MONGO_URI = os.environ.get("MONGO_URI")
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
@@ -16,4 +16,18 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/browse_recipes')
 def browse_recipes():
-    return render_template('browse.html', recipes=mongo.db.recipes.find())
+    return render_template("browse.html", recipes=mongo.db.recipes.find())
+
+@app.route('/search_recipes')
+def search_recipes():
+    return render_template("search.html", recipes=mongo.db.recipes.find())
+
+@app.route('/add_recipe', methods=['POST'])
+def add_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('search_recipes'))
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
+           )), debug=True)
