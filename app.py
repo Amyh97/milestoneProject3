66314d6047071510c55bab5fbea2b1177df81ad8
Amyh97 <amy.hollis@btinternet.com>
2020-10-22 12:13:28 +0000

    use javascript to build array for python

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if path.exists("env.py"):
     import env
-import eel
 
 
-eel.init('templates')
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'recipes'
 MONGO_URI = os.environ.get("MONGO_URI")
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
+recipes = mongo.db.recipes
 
 
 @app.route('/')
@@ -22,15 +21,9 @@ def browse_recipes():
     return render_template("browse.html")
 
 
-@app.route('/search_recipes')
-@eel.expose
+@app.route('/search_recipes', methods=["GET"])
 def search_recipes():
-    cuisine = eel.set_value()
-    protein = []
-    carbs = []
-    diet = []
-    allergies = []
-    return render_template('search.html', recipes=mongo.db.recipes.find({'cuisine': cuisine} or {'protein': protein} or {'carbs': carbs} or {'diet': diet} or {'allergies': allergies}))
+    return render_template('search.html')
 
 
 @app.route('/add_recipe')
@@ -39,5 +32,5 @@ def add_recipe():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
-           )), debug=True)
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
+            debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
-$('#italian').on('click', set_value(),{
-    cusisne = "italian"
-});
+var cuisine=[];
+
+$("#italian").innerHTML=cuisine;
+function add_cuisine() {
+        cuisine.push("italian");
+       console.log(cuisine);
+    }
+
+
+var carbs=[];
+
+$("#rice").innerHTML=carbs;
+function add_carbs() {
+        carbs.push("rice");
+       console.log(carbs);
+    } 
+$("#pasta").innerHTML=carbs;
+function add_carbs() {
+        carbs.push("pasta");
+       console.log(carbs);
+    }
