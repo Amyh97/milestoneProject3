e8794428a2d0861f5d558cbf39493cb92c879212
Amyh97 <amy.hollis@btinternet.com>
2020-11-30 17:54:50 +0000

    update script.js to only act on one card in results.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,10 @@
-$('.view').on('click', function(){
-    $('.recipe_card').removeClass('hidden') 
-    $('.overview').addClass('hidden')
-})
+$('button.view').on('click', function() {
+    $(this).parent('div.overview').addClass('hidden');
+    $(this).parent('div.overview').next('div.recipe_card').removeClass('hidden');
+});
 
-$('.close').on('click', function(){
-    $('.recipe_card').addClass('hidden')
-    $('.overview').removeClass('hidden')
-})
+$('.close').on('click', function() {
+    $(this).parent('div.recipe_card').addClass('hidden');
+    $(this).parent('div.recipe_card').prev('div.overview').removeClass('hidden');
+});
+
