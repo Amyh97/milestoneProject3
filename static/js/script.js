7b286044a96acc392907306e43e2d08d63eb6037
Amyh97 <amy.hollis@btinternet.com>
2020-12-14 18:46:55 +0000

    style cards, buttons, change background colour to white

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ $('button.view').on('click', function() {
     $(this).parent('div.overview').next('div.recipe_card').removeClass('hidden');
 });
 
-$('.close').on('click', function() {
+$('.closed').on('click', function() {
     $(this).parent('div.recipe_card').addClass('hidden');
     $(this).parent('div.recipe_card').prev('div.overview').removeClass('hidden');
 });
