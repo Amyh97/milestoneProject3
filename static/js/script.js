3c9c57e3b17da53f448caf18b443b2d83f147a56
Amyh97 <amy.hollis@btinternet.com>
2020-12-28 21:59:06 +0000

    add error message if no results returned from search

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+//show and hide card overviews and recipes
 $('button.view').on('click', function() {
     $(this).parent('div.overview').addClass('hidden');
     $(this).parent('div.overview').next('div.recipe_card').removeClass('hidden');
@@ -8,8 +9,18 @@ $('.closed').on('click', function() {
     $(this).parent('div.recipe_card').prev('div.overview').removeClass('hidden');
 });
 
+//carousel on browse.html
 $(document).ready(function() {
     $('#my-carousel').carousel({
       interval: 2000
     })
 })
+
+//show and hide error message on search page if no results
+if ($('.overview').length == 0) {
+    $('.error').removeClass("hidden")
+    console.log("empty")
+}
+else{
+    console.log("keep going")
+}
