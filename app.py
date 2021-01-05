e0ada59e2c889db82698c94dee0a6eac385433e9
Amyh97 <amy.hollis@btinternet.com>
2021-01-05 14:25:42 +0000

    add heading to results

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from os import path
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -15,6 +15,7 @@ MONGO_URI = os.environ.get('MONGO_URI')
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
 recipes = mongo.db.recipes
+app.config.update(SECRET_KEY=os.urandom(24))
 
 
 @app.route('/')
@@ -60,68 +61,72 @@ in browse.html and opens results.html """
 
 @app.route('/search_italian', methods=['GET'])
 def search_italian():
+    heading = "Italian Dishes"
     italian = mongo.db.recipes.find({'cuisine': 'italian'})
-    return render_template('results.html', recipes=italian)
+    return render_template('results.html', recipes=italian, heading=heading)
 
 
 @app.route('/search_rice', methods=['GET'])
 def search_rice():
+    heading = "Rice Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                            {'carbs': 'rice'}))
+                            {'carbs': 'rice'}), heading=heading)
 
 
 @app.route('/search_vegetarian', methods=['GET'])
 def search_vegetarian():
+    heading = "Vegetarian Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'diet': 'vegetarian'}))
+                        {'diet': 'vegetarian'}), heading=heading)
 
 
 @app.route('/search_pasta', methods=['GET'])
 def search_pasta():
+    heading = "Pasta Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'carbs': 'pasta'}))
+                        {'carbs': 'pasta'}), heading=heading)
 
 
 @app.route('/search_sweet', methods=['GET'])
 def search_sweet():
+    heading = "Sweet Treats"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'cuisine': 'sweet'}))
+                        {'cuisine': 'sweet'}), heading=heading)
 
 
 @app.route('/search_beef', methods=['GET'])
 def search_beef():
+    heading = "Beef Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'protein': 'beef'}))
+                        {'protein': 'beef'}), heading=heading)
 
 
 @app.route('/search_poultry', methods=['GET'])
 def search_poultry():
+    heading = "Poultry Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'protein': 'poultry'}))
-
-
-@app.route('/search_lactose', methods=['GET'])
-def search_lactose():
-    return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'diet': 'lactose free'}))
+                        {'protein': 'poultry'}), heading=heading)
 
 
 @app.route('/search_mexican', methods=['GET'])
 def search_mexican():
+    heading = "Mexican Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                        {'cuisine': 'mexican'}))
+                        {'cuisine': 'mexican'}), heading=heading)
 
 
 @app.route('/search_pork', methods=['GET'])
 def search_pork():
+    heading = "Pork Dishes"
     return render_template('results.html', recipes=mongo.db.recipes.find(
-                            {'protein': 'pork'}))
+                            {'protein': 'pork'}), heading=heading)
 
 
 # add recipe
 @app.route('/recipe/add', methods=['POST', 'GET'])
 def add_recipe():
     if request.method == "POST":
+        flash("Thank you, this recipe has been added.")
         new_recipe = {
             "name": request.form.get("name"),
             "cuisine": request.form.getlist("cuisine"),
@@ -129,21 +134,26 @@ def add_recipe():
             "carbs": request.form.getlist("carbs"),
             "diet": request.form.getlist("diet"),
             "allergies": request.form.getlist("allergies"),
-            "ingredients": request.form.getlist("ingredients"),
+            "ingredients": request.form.get("ingredients"),
             "method": request.form.get("method"),
             "notes": request.form.get("notes"),
             "image": request.form.get("image"),
             "source": request.form.get("source")
         }
         mongo.db.recipes.insert_one(new_recipe)
-    return render_template(
-        "add.html",
-        cuisine=mongo.db.cuisine.find(),
-        protein=mongo.db.protein.find(),
-        carbs=mongo.db.carbs.find(),
-        diet=mongo.db.diet.find(),
-        allergies=mongo.db.allergies.find(),
-    )
+        return redirect(url_for('recipes',
+                        cuisine=mongo.db.cuisine.find(),
+                        protein=mongo.db.protein.find(),
+                        carbs=mongo.db.carbs.find(),
+                        diet=mongo.db.diet.find(),
+                        allergies=mongo.db.allergies.find()))
+    else:
+        return render_template('add.html',
+                               cuisine=mongo.db.cuisine.find(),
+                               protein=mongo.db.protein.find(),
+                               carbs=mongo.db.carbs.find(),
+                               diet=mongo.db.diet.find(),
+                               allergies=mongo.db.allergies.find())
 
 
 # recipe edit
@@ -151,6 +161,7 @@ def add_recipe():
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     if request.method == 'POST':
+        flash("Thank you, this recipe has been updated.")
         mongo.db.recipes.update({'_id': ObjectId(recipe_id)}, {
             'name': request.form.get('name'),
             'cuisine': request.form.getlist('cuisine'),
@@ -164,15 +175,21 @@ def update_recipe(recipe_id):
             'image': request.form.get('image'),
             'source': request.form.get('source')
           })
-    print(recipes)
-    return render_template('update.html',
-                           recipes=recipes,
-                           cuisine=list(mongo.db.cuisine.find()),
-                           protein=list(mongo.db.protein.find()),
-                           carbs=list(mongo.db.carbs.find()),
-                           diet=list(mongo.db.diet.find()),
-                           allergies=list(mongo.db.allergies.find())
-                           )
+        return redirect(url_for('recipes',
+                                recipes=recipes,
+                                cuisine=list(mongo.db.cuisine.find()),
+                                protein=list(mongo.db.protein.find()),
+                                carbs=list(mongo.db.carbs.find()),
+                                diet=list(mongo.db.diet.find()),
+                                allergies=list(mongo.db.allergies.find())))
+    else:
+        return render_template('update.html',
+                               recipes=recipes,
+                               cuisine=mongo.db.cuisine.find(),
+                               protein=mongo.db.protein.find(),
+                               carbs=mongo.db.carbs.find(),
+                               diet=mongo.db.diet.find(),
+                               allergies=mongo.db.allergies.find())
 
 # delete recipe
 
@@ -180,7 +197,7 @@ def update_recipe(recipe_id):
 @app.route('/recipe/<recipe_id>/delete')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('browse_recipes'))
+    return redirect(url_for('recipes'))
 
 
 if __name__ == "__main__":
