7d8fd01a6675fb990e14ff2cc10dbc49ea52ff89
Amyh97 <amy.hollis@btinternet.com>
2021-01-07 16:06:29 +0000

    remove other cards from page while recipe is open, improve styling to cards

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 //show and hide card overviews and recipes
 $('button.view').on('click', function() {
-    $(this).parent('div.overview').addClass('hidden');
+    $('.overview').addClass('hidden');
     $(this).parent('div.overview').next('div.recipe_card').removeClass('hidden');
 });
 
 $('.closed').on('click', function() {
     $(this).parent('div.recipe_card').addClass('hidden');
-    $(this).parent('div.recipe_card').prev('div.overview').removeClass('hidden');
+    $('.overview').removeClass('hidden');
 });
 
 //carousel on browse.html
