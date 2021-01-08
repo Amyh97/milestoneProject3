e53c2adcccb41bea67b2c5909d04be4060588ad0
Amyh97 <amy.hollis@btinternet.com>
2021-01-08 16:52:24 +0000

    add comments to code, format and validate

--- app.py
+++ app.py
@@ -5,56 +5,62 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from os import path
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-if path.exists('env.py'):
+
+if path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'milestoneProject'
-MONGO_URI = os.environ.get('MONGO_URI')
-app.config['MONGO_URI'] = MONGO_URI
+debug = os.environ.get("DEBUG")
+app.config["MONGO_DBNAME"] = "milestoneProject"
+MONGO_URI = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 recipes = mongo.db.recipes
 app.config.update(SECRET_KEY=os.urandom(24))
 
 
-@app.route('/')
+@app.route("/")
 def browse_recipes():
     carousel = mongo.db.recipes.find()
-    recipes = (list(carousel)[-5:])
+    recipes = list(carousel)[-5:]
     category = mongo.db.categories.find()
-    return render_template('browse.html', recipes=recipes,
-                           category=category)
+    return render_template("browse.html", recipes=recipes, category=category)
 
 
 # search and browse
-@app.route('/recipes', methods=["POST", "GET"])
+@app.route("/recipes", methods=["POST", "GET"])
 def recipes():
+    # create query_object so not all fields need values
     query_object = {}
-    cuisine = request.form.getlist('cuisine')
-    protein = request.form.getlist('protein')
-    carbs = request.form.getlist('carbs')
-    diet = request.form.getlist('diet')
+    cuisine = request.form.getlist("cuisine")
+    protein = request.form.getlist("protein")
+    carbs = request.form.getlist("carbs")
+    diet = request.form.getlist("diet")
     if cuisine:
-        query_object['cuisine'] = {'$in': cuisine}
+        query_object["cuisine"] = {"$in": cuisine}
     if protein:
-        query_object['protein'] = {'$in': protein}
+        query_object["protein"] = {"$in": protein}
     if carbs:
-        query_object['carbs'] = {'$in': carbs}
+        query_object["carbs"] = {"$in": carbs}
     if diet:
-        query_object['diet'] = {'$in': diet}
+        query_object["diet"] = {"$in": diet}
     recipes = mongo.db.recipes.find(query_object)
-    return render_template('search.html', recipes=recipes,
-                           cuisine=mongo.db.cuisine.find(),
-                           protein=mongo.db.protein.find(),
-                           carbs=mongo.db.carbs.find(),
-                           diet=mongo.db.diet.find())
+    return render_template(
+        "search.html",
+        recipes=recipes,
+        cuisine=mongo.db.cuisine.find(),
+        protein=mongo.db.protein.find(),
+        carbs=mongo.db.carbs.find(),
+        diet=mongo.db.diet.find(),
+    )
 
 
 # add recipe
-@app.route('/recipe/add', methods=['POST', 'GET'])
+@app.route("/recipe/add", methods=["POST", "GET"])
 def add_recipe():
     if request.method == "POST":
+        # let users know submit was successful
         flash("Thank you, this recipe has been added.")
         new_recipe = {
             "name": request.form.get("name"),
@@ -67,64 +73,77 @@ def add_recipe():
             "method": request.form.get("method"),
             "notes": request.form.get("notes"),
             "image": request.form.get("image"),
-            "source": request.form.get("source")
+            "source": request.form.get("source"),
         }
         mongo.db.recipes.insert_one(new_recipe)
-        return redirect(url_for('recipes'))
+        return redirect(url_for("recipes"))
     else:
-        return render_template('add.html',
-                               cuisine=mongo.db.cuisine.find(),
-                               protein=mongo.db.protein.find(),
-                               carbs=mongo.db.carbs.find(),
-                               diet=mongo.db.diet.find(),
-                               allergies=mongo.db.allergies.find())
+        # renders page before form is submitted
+        return render_template(
+            "add.html",
+            cuisine=mongo.db.cuisine.find(),
+            protein=mongo.db.protein.find(),
+            carbs=mongo.db.carbs.find(),
+            diet=mongo.db.diet.find(),
+            allergies=mongo.db.allergies.find(),
+        )
 
 
 # recipe edit
-@app.route('/recipe/<recipe_id>/edit', methods=['GET', 'POST'])
+@app.route("/recipe/<recipe_id>/edit", methods=["GET", "POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    if request.method == 'POST':
+    if request.method == "POST":
+        # lets users know update was successful
         flash("Thank you, this recipe has been updated.")
-        mongo.db.recipes.update({'_id': ObjectId(recipe_id)}, {
-            'name': request.form.get('name'),
-            'cuisine': request.form.getlist('cuisine'),
-            'protein': request.form.getlist('protein'),
-            'carbs': request.form.getlist('carbs'),
-            'diet': request.form.getlist('diet'),
-            'allergies': request.form.getlist('allergies'),
-            'ingredients': request.form.get('ingredients'),
-            'method': request.form.get('method'),
-            'notes': request.form.get('notes'),
-            'image': request.form.get('image'),
-            'source': request.form.get('source')
-          })
-        return redirect(url_for('recipes',
-                                recipes=recipes,
-                                cuisine=list(mongo.db.cuisine.find()),
-                                protein=list(mongo.db.protein.find()),
-                                carbs=list(mongo.db.carbs.find()),
-                                diet=list(mongo.db.diet.find()),
-                                allergies=list(mongo.db.allergies.find())))
+        mongo.db.recipes.update(
+            {"_id": ObjectId(recipe_id)},
+            {
+                "name": request.form.get("name"),
+                "cuisine": request.form.getlist("cuisine"),
+                "protein": request.form.getlist("protein"),
+                "carbs": request.form.getlist("carbs"),
+                "diet": request.form.getlist("diet"),
+                "allergies": request.form.getlist("allergies"),
+                "ingredients": request.form.get("ingredients"),
+                "method": request.form.get("method"),
+                "notes": request.form.get("notes"),
+                "image": request.form.get("image"),
+                "source": request.form.get("source"),
+            },
+        )
+        return redirect(
+            url_for(
+                "recipes",
+                recipes=recipes,
+                cuisine=list(mongo.db.cuisine.find()),
+                protein=list(mongo.db.protein.find()),
+                carbs=list(mongo.db.carbs.find()),
+                diet=list(mongo.db.diet.find()),
+                allergies=list(mongo.db.allergies.find()),
+            )
+        )
     else:
-        return render_template('update.html',
-                               recipes=recipes,
-                               cuisine=mongo.db.cuisine.find(),
-                               protein=mongo.db.protein.find(),
-                               carbs=mongo.db.carbs.find(),
-                               diet=mongo.db.diet.find(),
-                               allergies=mongo.db.allergies.find())
+        # prevents redirect before changes are made
+        return render_template(
+            "update.html",
+            recipes=recipes,
+            cuisine=mongo.db.cuisine.find(),
+            protein=mongo.db.protein.find(),
+            carbs=mongo.db.carbs.find(),
+            diet=mongo.db.diet.find(),
+            allergies=mongo.db.allergies.find(),
+        )
+
 
 # delete recipe
-
-
-@app.route('/recipe/<recipe_id>/delete')
+@app.route("/recipe/<recipe_id>/delete")
 def delete_recipe(recipe_id):
+    # lets users know function was successful
     flash("Thank you, this recipe has been deleted.")
-    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('recipes'))
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    return redirect(url_for("recipes"))
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")))
--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,27 @@
-//show and hide card overviews and recipes
-$('button.view').on('click', function() {
-    $('.overview').addClass('hidden');
-    $(this).parent('div.overview').next('div.recipe_card').removeClass('hidden');
+//show and hide card overview and recipe cards
+$("button.view").on("click", function () {
+    $(".overview").addClass("hidden");
+    $(this).parent("div.overview").next("div.recipe_card").removeClass("hidden");
 });
 
-$('.closed').on('click', function() {
-    $(this).parent('div.recipe_card').addClass('hidden');
-    $('.overview').removeClass('hidden');
+$(".closed").on("click", function () {
+    $(this).parent("div.recipe_card").addClass("hidden");
+    $(".overview").removeClass("hidden");
 });
 
 //carousel on browse.html
-$(document).ready(function() {
-   $('.carousel').carousel({
-      interval: 3000,
-    })
-})
+$(document).ready(function () {
+    $(".carousel").carousel({
+        interval: 3000,
+    });
+});
 
 //show and hide error message on search page if no results
-if ($('.overview').length == 0) {
-    $('.error').removeClass("hidden")
+if ($(".overview").length == 0) {
+    $(".error").removeClass("hidden");
 }
 
 // close flash message
-$(document).ready(function(){
+$(document).ready(function () {
     $("#successMessage").remove();
-},25000);
-
+}, 15000);
